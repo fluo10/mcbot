6c5911bc5353f93b235edb7ca146fd22527266b2
fluo10 <fluo10.dev@fireturtle.net>
2024-04-24 07:50:44 +0900

    Implement simple state machine

--- src/index.ts
+++ src/index.ts
@@ -3,114 +3,79 @@ const util = require("util");
 const {pathfinder, Movements, goals } = require('mineflayer-pathfinder');
 const pvp = require('mineflayer-pvp').plugin;
 require("dotenv").config();
-
-const { mineflayer: mineflayerViewer } = require('prismarine-viewer')
-
 const options = {
-  host: process.env.MINECRAFT_HOST,
-  port: parseInt(process.env.MINECRAFT_PORT as string, 27000),
-  username: process.env.MINECRAFT_USER,
-  auth: process.env.MINECRAFT_AUTH,
-  password: process.env.MINECRAFT_PASSWORD,
+  host: process.env.MCBOT_HOST,
+  port: process.env.MCBOT_PORT as string,
+  username: process.env.MCBOT_USER,
+  password: process.env.MCBOT_PASSWORD,
+  auth: process.env.MCBOT_AUTH,
 }
+const {
+  StateTransition,
+  BotStateMachine,
+  BehaviorIdle,
+  BehaviorGetClosestEntity,
+  BehaviorLookAtEntity,
+  EntityFilters,
+  NestedStateMachine,
+} = require('mineflayer-statemachine');
 
+console.log(process.env.MCBOT_HOST);
 const bot = mineflayer.createBot(options);
 
 bot.on('kicked', console.log);
 bot.on('error', console.log);
-
 bot.loadPlugin(pathfinder);
 bot.loadPlugin(pvp);
 
 bot.once('spawn', () => {
-    mineflayerViewer(bot, { port: 3007, firstPerson: true }) // port is the minecraft server port, if first person is false, you get a bird's-eye view
-});
-
-function parse_message(message: string): string[] | undefined {
-  let args = message.split(" ");
-  var index = args.indexOf("@" + bot.username);
-  if (index === -1) {
-    return undefined;
-  } else {
-    args.splice(index, 1);
-  }
-  return args
-}
-
-
-
-bot.targetPos = null;
-bot.targetEntity = null;
-bot.nearestEnemy = null;
-bot.order = null;
-bot.once('spawn', () => {
-  bot.addChatPattern('echo', /echo/)
-  bot.addChatPattern('follow', /follow/)
-  bot.addChatPattern('stay', /stay/)
+  //    mineflayerViewer(bot, { port: 3007, firstPerson: true }) // port is the minecraft server port, if first person is false, you get a bird's-eye view
+  console.log(`Bot joined the game with username ${bot.username}.`);
   const defaultMove = new Movements(bot)
   defaultMove.allow1by1tower = false;
   defaultMove.allowFreeMotion = true;
   defaultMove.canDig = false;
   defaultMove.canOpenDoors;
   bot.pathfinder.setMovements(defaultMove)
+
+  const targets = {};
+  const idleState = new BehaviorIdle();
+  const getClosestPlayer = new BehaviorGetClosestEntity(bot, targets, EntityFilters().PlayersOnly);
+  const lookAtPlayersState = new BehaviorLookAtEntity(bot, targets);
+
+  const transitions = [
+    new StateTransition({
+      parent: idleState,
+      child: getClosestPlayer,
+      onTransition: () => bot.chat('hello')
+    }),
+    new StateTransition( {
+      parent: getClosestPlayer,
+      child: lookAtPlayersState,
+      onTransition: () => true
+    }),
+    new StateTransition({
+      parent: lookAtPlayersState,
+      child: idleState,
+      onTransition: () => bot.chat('goodby')
+    }),
+  ]
+  bot.on('whisper', (username: string, message: string) => {
+    if(messageIsValid(username, message)) {
+      if (message === "follow me") {
+        transitions[0].trigger();
+      } else if(message === "stay here") {
+        transitions[2].trigger();
+      }
+    }
+  });
+  const rootLayer = new NestedStateMachine(transitions, idleState);
+  const stateMachine = new BotStateMachine(bot, rootLayer);
+  console.log(`Started a state machine with ${stateMachine.transitions.length} transitions and ${stateMachine.states.length} states`)
+
 });
 
 function messageIsValid(username:string, message:string): boolean {
   return username !== bot.username;
 }
-
-bot.on('whisper:echo', (username: string, message: string) => {
-  if(messageIsValid(username, message)) {
-    bot.whisper(username, message);
-  }
-});
-
-bot.on('whisper:follow', (username: string, message: string) => {
-  if(messageIsValid(username, message)){
-    const player = bot.players[username]
-    if (!player) {
-      bot.whisper(username, "I can't see you.");
-      return
-    }
-    bot.whisper(username, "I will follow you.");
-    bot.targetEntity = player.entity;
-  }
-});
-
-
-bot.on('physicsTick', () => {
-  const filter = (e: any) => e.type === 'mob' && e.position.distanceTo(bot.entiry.position) < 1 && e.displayName !== 'Armor Stand';
-
-  bot.nearestEnemy = bot.nearestEntity(filter);
-  if (bot.shouldAttack) {
-    bot.pvp.attack(bot.nearestEnemy);
-  } else {
-    bot.pvp.stopAttack
-  }
-  if (bot.shouldGoToBed) {
-  }
-});
-
-bot.on('stoppedAttacking', () => {
-  bot.resumeMovementToTarget()
-});
-bot.on('attackedTarget', () => {
-  bot.pvp.attack(bot.pvp.target())
-});
-
-bot.resumeMovementToTarget = () => {
-  if (bot.targetEntity) {
-    bot.moveToTargerEntity(bot.targetEntity);
-  } else if (bot.targetPos) {
-    bot.moveToTargetPos(bot.targetPos);
-  }
-}
-
-bot.moveToTargetPos = (entity: any) => {
-  bot.pathfinder.setGoal(new goals.GoalFollow(entity, 8))
-}
-bot.moveToTargerEntity = (pos: any) =>{
-  bot.pathfinder.setGoal(new goals.GoalBlock(pos.x, pos.y, pos.z, 8))
-}
-
 
